21cf2762c964a51f59c81be207230b79090eceec
piyakker <ericlien86@gamil.com>
2022-11-02 15:23:11 +0800

    feat: add pagination

--- controller/products.js
+++ controller/products.js
@@ -6,6 +6,7 @@ const getAllProductStatic = async (req, res) => {
 }
 
 const getAllProduct = async (req, res) => {
+
   //find function
   const { featured, company, name, sort, fields } = req.query
   const queryObject = {}
@@ -29,11 +30,19 @@ const getAllProduct = async (req, res) => {
   } else {
     result = result.sort('createAt')
   }
+
   //select function
   if (fields) {
     const fieldsList = fields.split(',').join(' ')
     result = result.select(fieldsList)
   }
+
+  //pagination
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+  result = result.skip(skip).limit(limit)
+  
   const products  = await result
   res.status(200).json({ products, nbHits: products.length })
 }
