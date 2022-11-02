cd6f71e384ff292feb14ef75fe9b7cc9f61a920d
piyakker <ericlien86@gamil.com>
2022-11-02 14:59:08 +0800

    feat: add find function

--- controller/products.js
+++ controller/products.js
@@ -6,7 +6,21 @@ const getAllProductStatic = async (req, res) => {
 }
 
 const getAllProduct = async (req, res) => {
-  const products = await Product.find({})
+  //find function
+  const { featured, company, name } = req.query
+  const queryObject = {}
+  if (featured) {
+    queryObject.featured = featured
+  }
+  if (company) {
+    queryObject.company = company
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' }
+  }
+  console.log(queryObject)
+
+  const products = await Product.find(queryObject)
   res.status(200).json({ products, nbHits: products.length })
 }
 
